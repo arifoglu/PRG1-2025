a4402c127f26e63f4b2ba3cc891e9924b3329950
arifoglu <arifoglu2020@gmail.com>
2025-09-15 15:42:23 +0200

    files added

---
+++ a1_septembre/sept15/first.cpp
@@ -0,0 +1,20 @@
+/*
+Labo : 01
+Nom(s) :Arifoglu Abdulkadir
+Date : 15.09.2025
+But : me présenter via un programme cpp.
+Remarque(s) :
+*/
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "Arifoglu Abdulkadir" << endl;
+    cout << "Je sais programmer "<< endl;
+    cout << "Je crois qu'on va apprendre le programmation procédurale avec C++" << endl;
+
+    return EXIT_SUCCESS;
+}
